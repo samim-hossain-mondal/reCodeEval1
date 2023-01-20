2f8dd6623ce551172b82f97a2259bc7f15acf4ec
samim-hossain-mondal <Samim_Hossain_Mondal@mckinsey.com>
2023-01-20 09:50:48 +0530

    feat: add functionality to calc total score and find best score in a set of games

--- bowlingGame.js
+++ bowlingGame.js
@@ -1,49 +1,49 @@
 // Feature 1 --> Calculate Total Score
 const calculateTotalScore = (rolls) => {
-    if (rolls.length > 21) {
-        throw new Error("Invalid Input");
-    }
-    let totalScore = 0;
-    let index = 0, countFrames = 0;
-    while (index < rolls.length) {
-        if (rolls[index] === 10) {
-            totalScore += (10 + rolls[index + 1] + rolls[index + 2]);
-            if (index === rolls.length - 3) {
-                countFrames += 1;
-                break;
-            }
-            index += 1;
-        }
-        else if (rolls[index] + rolls[index + 1] === 10) {
-            totalScore += (10 + rolls[index + 2]);
-            if (index === rolls.length - 3) {
-                countFrames += 1;
-                break;
-            }
-            index += 2;
-        }
-        else {
-            if ((rolls[index] + rolls[index + 1]) > 10) {
-                throw new Error('Invalid Input');
-            }
-            totalScore += (rolls[index] + rolls[index + 1]);
-            index += 2;
-        }
+  if (rolls.length > 21) {
+    throw new Error('Invalid Input');
+  }
+  let totalScore = 0;
+  let index = 0, countFrames = 0;
+  while (index < rolls.length) {
+    if (rolls[index] === 10) {
+      totalScore += (10 + rolls[index + 1] + rolls[index + 2]);
+      if (index === rolls.length - 3) {
         countFrames += 1;
+        break;
+      }
+      index += 1;
     }
-    if (countFrames !== 10 || isNaN(totalScore)) {
+    else if (rolls[index] + rolls[index + 1] === 10) {
+      totalScore += (10 + rolls[index + 2]);
+      if (index === rolls.length - 3) {
+        countFrames += 1;
+        break;
+      }
+      index += 2;
+    }
+    else {
+      if ((rolls[index] + rolls[index + 1]) > 10) {
         throw new Error('Invalid Input');
+      }
+      totalScore += (rolls[index] + rolls[index + 1]);
+      index += 2;
     }
-    return totalScore;
+    countFrames += 1;
+  }
+  if (countFrames !== 10 || isNaN(totalScore)) {
+    throw new Error('Invalid Input');
+  }
+  return totalScore;
 };
 
 // Feature 2 --> Find Best Score in set of Games
 const bestScore = (gameArray) => {
-    let maxScore = calculateTotalScore(gameArray[0]);
-    return gameArray.reduce((acc, cuu) => {
-        acc = Math.max(acc, calculateTotalScore(cuu));
-        return acc;
-    }, maxScore);
+  let maxScore = calculateTotalScore(gameArray[0]);
+  return gameArray.reduce((acc, cuu) => {
+    acc = Math.max(acc, calculateTotalScore(cuu));
+    return acc;
+  }, maxScore);
 };
 
 module.exports = { calculateTotalScore, bestScore };
